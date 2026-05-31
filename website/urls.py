708f08a093cc244ff8354e863ac71bafbbf91242
Ariya <rastpourariya@gmail.com>
2026-05-31 20:39:23 +0330

    Dinamic urls
    I finished 70% of urls in my project. but onething is wrong , contact url in mobile page is not working..

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from website.views import Home_view ,About_view ,Contact_view
 
+app_name = 'website'
+
 urlpatterns = [
-    path('', Home_view),
-    path('about/', About_view),
-    path('contact/', Contact_view)
+    path('', Home_view, name='home'),
+    path('about/', About_view, name='about'),
+    path('/contact/', Contact_view, name='contact')
 ]
