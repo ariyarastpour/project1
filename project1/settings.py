51d4b484889e919f57df5c350831c52e58ae0b4f
Ariya <rastpourariya@gmail.com>
2026-05-31 15:34:31 +0330

    block and endblock
    I created a base.html file and put footer in that and with 'block  <name>' i used that..

--- project1/settings.py
+++ project1/settings.py
@@ -116,3 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MSDIA_URL = 'media/'
+MSDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics'
+]
