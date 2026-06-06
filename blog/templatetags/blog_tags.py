c16c6308c240ce2b7a7e086f47771514532dae73
Ariya <rastpourariya@gmail.com>
2026-06-07 01:42:05 +0330

    ending category
    I Completely finished categories in my project

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -22,3 +22,21 @@ def posts_categories():
     for name in categories:
         cat_dict[name]=posts.filter(category=name).count()
     return {'categories':cat_dict}
+
+@register.inclusion_tag('blog/blog-categories-footer.html')
+def posts_categories_footer():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories':cat_dict}
+
+@register.inclusion_tag('blog/blog-categories-nav.html')
+def posts_categories_header():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return {'categories':cat_dict}
