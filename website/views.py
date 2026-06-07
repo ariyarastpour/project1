0cc51f6b74f8c6bf29aab86554da641bb8848dcc
Ariya <rastpourariya@gmail.com>
2026-06-07 15:28:41 +0330

    Paginations
    I finished my Paginations but still buttons( previous , next ) is wrong.

--- website/views.py
+++ website/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from blog.views import Post
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+
 
 def Home_view(request):
     posts = Post.objects.filter(status=1)
     first_post = posts.first()
+    other_posts = posts[1:]
+    posts = Paginator(posts,5)
+
+    #paginations
+    try:
+       page_number = request.GET.get('page')
+       posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+    first_post = posts[0]
     other_posts = posts[1:]
 
     context = {
